501b3be4a64dfc5ef39bddc65a4fb8dd89b0ee99
Sabbir Hosen <18201076@uap-bd.edu>
2021-10-30 01:55:37 +0600

    7 test case done

--- TestClass.py
+++ TestClass.py
@@ -50,6 +50,216 @@ class Test:
                 screenshot=True
             )
 
+    def test_case_2(self, city, name):
+        try:
+            # Start of Test
+            self.report.write_step(
+                'Search  doctor based on city and name',
+                status=self.report.status.Start,
+                test_number=2
+            )
+            self.driver.get(self.url)
+            self.driver.find_element(By.XPATH, '/html/body/section[1]/div/div/div[2]/div/ul/li[1]/button').click()
+            self.driver.find_element(By.XPATH, '/html/body/section[2]/div/div/div/div/div[1]/ul/li[1]/button').click()
+            self.driver.find_element(By.ID, 'cityField').send_keys(city)
+            self.driver.find_element(By.ID, 'doctorSpeField').send_keys(name)
+            button = self.driver.find_element(By.XPATH, '//*[@id="pills-appointment"]/div/div[1]/form/button')
+            self.driver.execute_script("arguments[0].click();", button)
+            r_name = self.driver.find_element(By.XPATH, '/html/body/div[2]/section/div/div/table/tbody/tr[1]/td[1]/a').text
+            r_city = self.driver.find_element(By.XPATH, '/html/body/div[2]/section/div/div/table/tbody/tr[1]/td[3]').text
+
+            assert ((r_name == name) and (r_city == city))
+            self.report.write_step(
+                'Search  doctor matched on city and name',
+                status=self.report.status.Pass,
+                screenshot=True
+            )
+        except AssertionError:
+            self.report.write_step(
+                'Search  doctor not matched on city and name',
+                status=self.report.status.Fail,
+                screenshot=True
+            )
+        except Exception as e:
+            self.report.write_step(
+                f'Something went wrong during execution!</br>{e}',
+                status=self.report.status.Warn,
+                screenshot=True
+            )
+
+    def test_case_3(self, city):
+        try:
+            self.report.write_step(
+                'Search  doctor based only city',
+                status=self.report.status.Start,
+                test_number=3
+            )
+            self.driver.get(self.url)
+            self.driver.find_element(By.XPATH, '/html/body/section[1]/div/div/div[2]/div/ul/li[1]/button').click()
+            self.driver.find_element(By.XPATH, '/html/body/section[2]/div/div/div/div/div[1]/ul/li[1]/button').click()
+            self.driver.find_element(By.ID, 'cityField').send_keys(city)
+            button = self.driver.find_element(By.XPATH, '//*[@id="pills-appointment"]/div/div[1]/form/button')
+            self.driver.execute_script("arguments[0].click();", button)
+            r_city = self.driver.find_element(By.XPATH, '/html/body/div[2]/section/div/div/table/tbody/tr[1]/td[3]').text
+
+            assert (r_city == city)
+            self.report.write_step(
+                'Search  doctor matched on',
+                status=self.report.status.Pass,
+                screenshot=True
+            )
+        except AssertionError:
+            self.report.write_step(
+                'Search  doctor not matched on city',
+                status=self.report.status.Fail,
+                screenshot=True
+            )
+        except Exception as e:
+            self.report.write_step(
+                f'Something went wrong during execution!</br>{e}',
+                status=self.report.status.Warn,
+                screenshot=True
+            )
+
+    def test_case_4(self, name):
+        try:
+            self.report.write_step(
+                'Search  doctor based on name',
+                status=self.report.status.Start,
+                test_number=4
+            )
+            self.driver.get(self.url)
+            self.driver.find_element(By.XPATH, '/html/body/section[1]/div/div/div[2]/div/ul/li[1]/button').click()
+            self.driver.find_element(By.XPATH, '/html/body/section[2]/div/div/div/div/div[1]/ul/li[1]/button').click()
+            self.driver.find_element(By.ID, 'doctorSpeField').send_keys(name)
+            button = self.driver.find_element(By.XPATH, '//*[@id="pills-appointment"]/div/div[1]/form/button')
+            self.driver.execute_script("arguments[0].click();", button)
+            r_name = self.driver.find_element(By.XPATH, '/html/body/div[2]/section/div/div/table/tbody/tr[1]/td[1]/a').text
+
+            assert (r_name == name)
+            self.report.write_step(
+                'Search  doctor matched on name',
+                status=self.report.status.Pass,
+                screenshot=True
+            )
+        except AssertionError:
+            self.report.write_step(
+                'Search  doctor not matched on name',
+                status=self.report.status.Fail,
+                screenshot=True
+            )
+        except Exception as e:
+            self.report.write_step(
+                f'Something went wrong during execution!</br>{e}',
+                status=self.report.status.Warn,
+                screenshot=True
+            )
+
+    def test_case_5(self, city, hospital_name):
+        try:
+            # Start of Test
+            self.report.write_step(
+                'Search  Hospital based on city and name',
+                status=self.report.status.Start,
+                test_number=5
+            )
+            self.driver.get(self.url)
+            self.driver.find_element(By.XPATH, '/html/body/section[1]/div/div/div[2]/div/ul/li[2]/button').click()
+            self.driver.implicitly_wait(1)
+            self.driver.find_element(By.ID, 'cityField2').send_keys(city)
+            self.driver.find_element(By.ID, 'hospitalField').send_keys(hospital_name)
+            button = self.driver.find_element(By.XPATH, '/html/body/section[2]/div/div/div/div/div[2]/div/div[1]/form/button')
+            self.driver.execute_script("arguments[0].click();", button)
+            r_name = self.driver.find_element(By.XPATH, '/html/body/div[2]/section/div/div/table/tbody/tr[1]/td[1]/a').text
+            r_city = self.driver.find_element(By.XPATH, '/html/body/div[2]/section/div/div/table/tbody/tr[1]/td[3]').text
+
+            assert ((r_name == hospital_name) and (r_city == city))
+            self.report.write_step(
+                'Search hospital matched on city and name',
+                status=self.report.status.Pass,
+                screenshot=True
+            )
+        except AssertionError:
+            self.report.write_step(
+                'Search hospital not matched on city and name',
+                status=self.report.status.Fail,
+                screenshot=True
+            )
+        except Exception as e:
+            self.report.write_step(
+                f'Something went wrong during execution!</br>{e}',
+                status=self.report.status.Warn,
+                screenshot=True
+            )
+
+    def test_case_6(self, city):
+        try:
+            self.report.write_step(
+                'Search  Hospital based on city',
+                status=self.report.status.Start,
+                test_number=6
+            )
+            self.driver.get(self.url)
+            self.driver.find_element(By.XPATH, '/html/body/section[1]/div/div/div[2]/div/ul/li[2]/button').click()
+            self.driver.implicitly_wait(1)
+            self.driver.find_element(By.ID, 'cityField2').send_keys(city)
+            button = self.driver.find_element(By.XPATH, '/html/body/section[2]/div/div/div/div/div[2]/div/div[1]/form/button')
+            self.driver.execute_script("arguments[0].click();", button)
+            r_city = self.driver.find_element(By.XPATH, '/html/body/div[2]/section/div/div/table/tbody/tr[1]/td[3]').text
+
+            assert (r_city == city)
+            self.report.write_step(
+                'Search hospital matched on city',
+                status=self.report.status.Pass,
+                screenshot=True
+            )
+        except AssertionError:
+            self.report.write_step(
+                'Search hospital not matched on city',
+                status=self.report.status.Fail,
+                screenshot=True
+            )
+        except Exception as e:
+            self.report.write_step(
+                f'Something went wrong during execution!</br>{e}',
+                status=self.report.status.Warn,
+                screenshot=True
+            )
+
+    def test_case_7(self, hospital_name):
+        try:
+            self.report.write_step(
+                'Search  Hospital based on name',
+                status=self.report.status.Start,
+                test_number=7
+            )
+            self.driver.get(self.url)
+            self.driver.find_element(By.XPATH, '/html/body/section[1]/div/div/div[2]/div/ul/li[2]/button').click()
+            self.driver.implicitly_wait(1)
+            self.driver.find_element(By.ID, 'hospitalField').send_keys(hospital_name)
+            button = self.driver.find_element(By.XPATH, '/html/body/section[2]/div/div/div/div/div[2]/div/div[1]/form/button')
+            self.driver.execute_script("arguments[0].click();", button)
+            r_name = self.driver.find_element(By.XPATH, '/html/body/div[2]/section/div/div/table/tbody/tr[1]/td[1]/a').text
+
+            assert (r_name == hospital_name)
+            self.report.write_step(
+                'Search hospital matched on name',
+                status=self.report.status.Pass,
+                screenshot=True
+            )
+        except AssertionError:
+            self.report.write_step(
+                'Search hospital not matched on name',
+                status=self.report.status.Fail,
+                screenshot=True
+            )
+        except Exception as e:
+            self.report.write_step(
+                f'Something went wrong during execution!</br>{e}',
+                status=self.report.status.Warn,
+                screenshot=True
+            )
+
     def generate_report(self):
         self.report.generate_report()
         self.driver.quit()
--- app.py
+++ app.py
@@ -2,5 +2,18 @@ from TestClass import Test
 
 url = 'http://127.0.0.1:8000/'
 test = Test(url=url)
-test.landingHomePage()
+# # Landing page status
+# test.landingHomePage()
+# # Search  doctor based on city and name
+# test.test_case_2(city='Dhaka', name='Dr. Abdul Mannan Sarker')
+# # Search  doctor based only city
+# test.test_case_3(city='Dhaka')
+# # Search  doctor based only name
+# test.test_case_4(name='Dr. Abdul Mannan Sarker')
+# # Search  hospital based on city and name
+# test.test_case_5(hospital_name='AFMC - Armed Forces Medical College', city='Dhaka')
+# # Search  hospital based on city
+# test.test_case_6(city='Dhaka')
+# # Search  hospital based on name
+# test.test_case_7(hospital_name='AFMC - Armed Forces Medical College')
 test.generate_report()
